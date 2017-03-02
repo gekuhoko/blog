aa6c8dac2e105978756b09781233e1f830f42090
gekuhoko <gerhard@gotarot.com.hk>
2017-03-02 13:55:07 +0800

    remove line break for iphone 5

--- resources/views/partials/navigationBar.blade.php
+++ resources/views/partials/navigationBar.blade.php
@@ -3,19 +3,19 @@
     <div class="container">
         <div style="float: right; padding-top:4px; margin-right: -12px;">
             @if(strpos(Request::path(), 'blog') !== false)
-                <a class="btn" href="/" style="background-color: #EB5634; color: #ffffff; font-weight:900; padding-left: 30px; padding-right: 30px;">Profile</a>
+                <a class="btn" href="/" style="background-color: #EB5634; color: #ffffff; font-weight:900; padding-left: 14px; padding-right: 15px;">Profile</a>
             @else
-                <a class="btn" href="#" style="background-color: #EB5634; color: #ffffff; font-weight:900; padding-left: 30px; padding-right: 30px;" data-toggle="modal" data-target="#contactModal">Contact</a>
+                <a class="btn" href="#" style="background-color: #EB5634; color: #ffffff; font-weight:900; padding-left: 14px; padding-right: 15px;" data-toggle="modal" data-target="#contactModal">Contact</a>
             @endif
         </div>
         <div class="navbar-header page-scroll">
             @if(strpos(Request::path(), 'blog') !== false)
                 {{-- Headline if blog page --}}
-                <a class="navbar-brand page-scroll" href="/" style="font-weight:200; font-size:18px;">{{config('owner.name')}}</a>
+                <a class="navbar-brand page-scroll" href="/" style="font-weight:200; font-size:16px;">{{config('owner.name')}}</a>
                 <a class="hidden-xs navbar-brand page-scroll" href="/" style="margin-top:5px; font-size:11px">{{config('owner.title')}}</a>
             @else
                 {{-- Headline if profile page --}}
-                <a class="navbar-brand page-scroll" href="/blog" style="font-weight:200; font-size:18px;">{{config('owner.name')}}</a>
+                <a class="navbar-brand page-scroll" href="/blog" style="font-weight:200; font-size:16px;">{{config('owner.name')}}</a>
                 <a class="hidden-xs navbar-brand page-scroll" href="/blog" style="margin-top:5px; font-size:11px">{{config('owner.title')}}</a>
             @endif
         </div>
