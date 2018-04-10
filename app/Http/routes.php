0284448bb59550f76afe4359f826add5611d5f87
Gerhard Kuschnik <kuschnik.gerhard@gmail.com>
2018-04-10 08:22:55 +0800

    added bugsnag

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,27 +1,4 @@
 <?php
-/*
-    why this project?
-        I want to build a system that allows free creators build a following
-
-            OWN FOLLOWERS DATA
-                We need a separate system because you want to own your followings contacts, namely
-                    -> name
-                    -> deviceID
-                    -> email
-                    -> phone
-                If you just build a following on YT or IG or FB you get squeezed over time
-
-            CROSS PLATFORM BI
-                    -> check how fast you grow
-                    -> check growth drivers (platforms, topics)
-
-            HAVE CROSS PLATFORM DISTRIBUTION
-                It's like a pimped Wordpress but also has integration with IG, YT, Podcast
-
-
-    I need that system to build networking-equity.
-    This system is useful for friends as well
-*/
 
 Route::group([
     'middleware' => ['auth.basic', 'csrf'],
@@ -35,64 +12,57 @@ Route::controller('api/push-notification', 'PushNotificationController');
 Route::controller('push-notification', 'PushNotificationController');
 Route::get('worker.js', 'PushNotificationController@getWorkerScript');
 
-//Route::controller('/', 'PageController');
-
 Route::get('/', 'PageController@getIndex');
 Route::get('blog', 'PageController@getBlog');
 Route::get('{fallback}', 'PageController@fallback');
 
 
 
-/* Roadmap:
-    Web push fix
-    Google analytics prepare
-    Landing page should be blog
-    Posts only have soft deletion
-    Send web push not on publish but on extra send button (because want to preview first)
+/*
+ *  Execution structure:
+ *      - one article per week (wednesday 11:00)
+ *      - some coding
+ *      - some social media activity
+ *      - some learning (Gary Vee book? FL-hacks?)
+            - strategy (cloud)
+            - tactics (dirt)
+
+Next items
+    CODE FIRST
 
 
+        - Integrate BugSnag
 
+        - Decouple creation from push sending
+        - Soft deletion of posts with confirm button
+        - add article link to backend
 
+    WRITE
 
+    CODE AFTER
+        - improve contact form
+            - also send SMS / push notification to me
+            - also add phone for them (so i can call)
+        - Add scheduled publishing and reminder for me to write
+        - Add share-bar like in financial samurai
+        - Add signup via email / sms
+        - Check SEO improvements
 
+(AUTO)Distribution to target (biz owners)
+    -> Quora (VIDEO?)
+    -> Reddit
+    -> Youtube
+    -> Podcast?
+    -> IG
+    -> Facebook
+    -> LinkedIn
+    -> AngelList
+    -> Gruenderszene
+    -> DigitalCompact
+    -> SEM, SEO
 
+KPIs: - Visitors / day (network size)
+     - Devices registered (long term reach)
+     - Visit to contact-me conversion
 
-
-
-
-
-
-        Write cool PPC ads
-        Put PPC ads online
-        Make contact me form work
-        Check Google analytics
-
-    Write about technology stack
-
-    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    preview post
-
-    Distribution:
-    Add preview
-    Add article link
-    IG Autopost
-    Facebook Autopost
-
-    Add preview to post creation
-
-    Automatic distribution
-    8. FB Post
-    9. LI Post
-    10. IG Post
-    11. YT Post
-    12. Podcast / Audio
-    13...?
-
-    Add SMS signup + random switch + unsubscribe
-
-    BI
-    ...
-
-    Virality with sharing links
 */
--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -156,6 +157,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
